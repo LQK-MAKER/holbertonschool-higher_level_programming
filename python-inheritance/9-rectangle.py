3e9e7234c2ac5659aa4f588b5e12a95d51c3fe08
LQK-MAKER <lesmayoux.theo@gmail.com>
2025-02-05 11:41:59 +0100

    task 9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+the class Rectangle from BaseGeometry:
+- area :
+- str
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    the class Rectangle
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__height = height
+        self.__width = width
+
+    def area(self):
+        return self.__height µ self.__width
+
+    def __str__(self):
+        return "[Rectangle] {} {}".format(self.__width , self.__height)
