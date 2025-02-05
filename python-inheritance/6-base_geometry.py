878940d595ed9d807384c4c736d30f24cd14652f
LQK-MAKER <lesmayoux.theo@gmail.com>
2025-02-05 11:12:40 +0100

    task 6 and test

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+the class BaseGeometry:
+- area :
+"""
+
+
+class BaseGeometry:
+    """
+    class BaseGeometry
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
