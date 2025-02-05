4b61ed90af3cb20ce11a8efc85225a0a547f643a
LQK-MAKER <lesmayoux.theo@gmail.com>
2025-02-05 10:53:23 +0100

    task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+The programme return:
+- True
+- False
+"""
+
+
+class MyList(list):
+    """
+    mylist
+    """
+    def print_sorted(self):
+        print(sorted(self))
