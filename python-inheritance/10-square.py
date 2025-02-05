b8a209d9b021151044083ed984d77b7f1349102e
LQK-MAKER <lesmayoux.theo@gmail.com>
2025-02-05 11:48:34 +0100

    task 10 ans correcxtio,

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+write the class Square from Rectangle:
+- area :
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    the class Square
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Rectangle] {} {}".format(self.__size, self.__size)
