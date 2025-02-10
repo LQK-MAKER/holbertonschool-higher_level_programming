8c6571afc68b33be175a679a225002a24a45cbde
LQK-MAKER <lesmayoux.theo@gmail.com>
2025-02-10 13:31:37 +0100

    coreecetesdgb

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -2,21 +2,28 @@
 import csv
 import json
 """
-This module provides functions for serializing and deserializing."""
+This module provides functions for serializing and deserializing.
+"""
 
 
-def convert_csv_to_json(csv_filename):
+def convert_csv_to_json(csv_filename: str) -> bool:
     """
-    This function converts a CSV file to a JSON file.
+     This function converts a CSV file to a JSON file.
     """
+    if not csv_filename.endswith('.csv'):
+        raise ValueError("The file must have a .csv extension.")
 
     try:
         with open(csv_filename, 'r') as csv_file:
             reader = csv.DictReader(csv_file)
             data = [row for row in reader]
-        with open(csv_filename.replace('.csv', '.json'), 'w') as json_file:
+
+        with open("data.json", 'w') as json_file:
             json.dump(data, json_file, indent=4)
 
-            return True
+        return True
     except FileNotFoundError:
+        return False
+    except (PermissionError, OSError) as e:
+        print(f"An error occurred: {e}")
         return False
