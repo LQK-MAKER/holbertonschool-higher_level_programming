85d66e2fa37fe91f46230ba8f1bcb6497269f173
LQK-MAKER <lesmayoux.theo@gmail.com>
2025-02-10 12:30:57 +0100

    test

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -3,3 +3,20 @@ import csv
 import json
 """
 This module provides functions for serializing and deserializing."""
+
+
+def convert_csv_to_json(csv_filename):
+    """
+    This function converts a CSV file to a JSON file.
+    """
+
+    try:
+        with open(csv_filename, 'r') as csv_file:
+            reader = csv.DictReader(csv_file)
+            data = [row for row in reader]
+        with open(csv_filename.replace('.csv', '.json'), 'w') as json_file:
+            json.dump(data, json_file, indent=4)
+
+            return True
+    except FileNotFoundError:
+        return False
